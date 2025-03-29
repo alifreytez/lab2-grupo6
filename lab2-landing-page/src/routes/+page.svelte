872b00f747c65740b3a17dfb61905e29eeff2d89
alirio freytez <aliriofreytez1@gmail.com>
2025-03-29 12:56:58 -0400

    nuevo intento de reajuste del gitignore

---
+++ lab2-landing-page/src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<script>
+    import '../lib/styles/app.css';
+    import BienvenidoHome from "../lib/components/home/BienvenidoHome.svelte";
+    import Contactos from "../lib/components/Contactos.svelte"
+   
+	
+
+</script>
+
+<h1>Bienvenido a la página de inicio AQUI VA CABECERA </h1>
+
+<slot /> 
+<BienvenidoHome />
+<Contactos />
